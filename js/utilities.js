09f3920b0c52c805181ba90071b840c01ae22323
Aidan-Smyth <71399934+Aidan-Smyth@users.noreply.github.com>
2020-12-02 03:10:16 -0500

    Adjusted the speed and timing of stars and enemies, plus stops above-screen flight

--- js/utilities.js
+++ js/utilities.js
@@ -2,3 +2,5 @@ function randomFloatRange(min, max)
 {
 	return (Math.random() * (max - min + 1) + min);
 }
+
+//I wanted to make a function to pick a random value from a list, but that is not a 3am task
