0cfc17bb9061ff25163efac85f57313fac66f1da
Aidan-Smyth <71399934+Aidan-Smyth@users.noreply.github.com>
2020-12-21 01:18:12 -0500

    Improves ghost visuals, plus minor tweaks
    Ghosts are now opaque and have 7 possible faces. Ghosts take 4 seconds to spawn instead of 2. Ghost prevent overlap a second time to make weird edge cases less common. Added a utility function for random integers between 0 and a number.

--- js/utilities.js
+++ js/utilities.js
@@ -3,4 +3,8 @@ function randomFloatRange(min, max)
 	return (Math.random() * (max - min + 1) + min);
 }
 
-//I wanted to make a function to pick a random value from a list, but that is not a 3am task
+// Blatantly stolen from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) 
+{
+  return Math.floor(Math.random() * Math.floor(max));
+}
