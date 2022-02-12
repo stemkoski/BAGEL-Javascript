bf1aa76537a1ecae85173838b5a8f26069a67bfe
Lee Stemkoski <stemkoski@gmail.com>
2022-02-12 13:48:30 -0500

    updating documentation

---
+++ docs/examples/bagel/index.js
@@ -0,0 +1,11 @@
+// collect BAGEL framework classes
+
+export { Vector }    from "./Vector.js";
+export { Rectangle } from "./Rectangle.js";
+export { Texture }   from "./Texture.js";
+export { Sprite }    from "./Sprite.js";
+export { Group }     from "./Group.js";
+export { Screen }    from "./Screen.js";
+export { Game }      from "./Game.js";
+export { Input }     from "./Input.js";
+export { Clock }     from "./Clock.js";
