1c72dccc2bd8a2002671d0d0b3d79a77a5dc2827
Lee Stemkoski <stemkoski@gmail.com>
2022-02-26 23:24:48 -0500

    added Action support
    note: only Actions with referencing Label variables/methods will work (move, fade, destroy, etc.)

--- bagel/Label.js
+++ bagel/Label.js
@@ -14,8 +14,7 @@ class Label
 	{
 		this.text = " ";
 
-		this.x = 0;
-		this.y = 0;
+		this.position = new BAGEL.Vector();
 		this.alignment = "left"; // "left" | "center" | "right"
 
 		// style options, configured by setProperties method		
@@ -32,6 +31,13 @@ class Label
 		this.drawShadow = true;
 		this.shadowColor = "black";
 		this.shadowSize = 4;
+
+		// extra graphics options
+		this.visible = true;
+		this.opacity = 1.00;
+
+		// list of Actions: functions applied to object over time
+		this.actionList = [];
 	}
 
     /**
@@ -42,8 +48,7 @@ class Label
      */	
 	setPosition(x,y, alignment="left")
 	{
-		this.x = x;
-		this.y = y;
+		this.position.setValues(x, y);
 		this.alignment = alignment;
 	}
 
@@ -80,10 +85,84 @@ class Label
 		}
 	}
 
-	// must include, since called by containing Group
+	/**
+     * Change this sprite's position by the given amounts.
+     * @param {number} dx - value to add to the position x coordinate
+     * @param {number} dy - value to add to the position y coordinate
+     */
+	moveBy(dx, dy)
+	{
+		this.position.addValues(dx, dy);
+	}	
+
+	/**
+     * Change the opacity when drawing, 
+     *   enabling objects underneath to be partially visible
+     *   by blending their colors with the colors of this object.
+     * <br>
+     * 0 = fully transparent (appears invisible);  1 = fully opaque (appears solid) 
+     * @param {number} opacity - opacity of this object
+     */
+    setOpacity(opacity)
+	{
+		this.opacity = opacity;
+	}
+
+	/**
+     * Set whether this sprite should be visible or not;
+     *  determines whether sprite will be drawn on canvas.
+     * @param {boolean} visible - determines if this sprite is visible or not
+     */
+    setVisible(visible)
+	{
+		this.visible = visible;
+	}
+
+	/**
+	 * Remove this sprite from the group that contains it.
+	 */
+	destroy()
+	{
+		this.parentGroup.removeSprite(this);
+	}
+
+
+	/**
+	 * Add an {@link Action} to this object: a special function that
+	 *  will be automatically applied over time until it is complete.
+	 * <br>
+	 * Most common actions can be created with the static methods in the
+	 * {@link ActionFactory} class.
+	 * <br>
+	 * All actions added to this object are performed in parallel, unless
+	 *  enclosed by a {@link ActionFactory#sequence|Sequence} action.
+	 * @param {Action} action - an action to be applied to this object
+	 */
+	addAction(action)
+	{
+		this.actionList.push(action);
+	}
+
+	/**
+	 *  Process any {@link Action|Actions} that have been added to this Label.
+     *  @param {number} deltaTime - time elapsed since previous frame
+	 */
 	update(deltaTime)
 	{
-
+		// Update all actions (in parallel, by default).
+        // Using a copy of the list to avoid skipping the next action in the list
+        //  when the previous action is removed.
+		let actionListCopy = this.actionList.slice();
+		for (let action of actionListCopy)
+		{
+			let finished = action.apply(this, deltaTime);
+			if (finished)
+			{
+				let index = this.actionList.indexOf(action);
+				if (index > -1)
+					this.actionList.splice(index, 1);
+			}
+		}
 	}
 
 	/**
@@ -100,7 +179,7 @@ class Label
 		context.textAlign = this.alignment;
 		
 		context.setTransform(1,0, 0,1, 0,0); 
-		context.globalAlpha = 1.00;
+		context.globalAlpha = this.opacity;
 
 		// shadow draw settings
 		if (this.drawShadow)
@@ -110,20 +189,20 @@ class Label
 		}
 
 		// draw filled in text (multiple times to increase drop shadow intensity)
-		context.fillText( this.text, this.x, this.y );
-		context.fillText( this.text, this.x, this.y );
+		context.fillText( this.text, this.position.x, this.position.y );
+		context.fillText( this.text, this.position.x, this.position.y );
 
 		// disable shadowBlur, otherwise all sprites drawn later will have shadows
 		context.shadowBlur = 0;
 		// draw filled text again, to fill over interior shadow blur
-		context.fillText( this.text, this.x, this.y );
+		context.fillText( this.text, this.position.x, this.position.y );
 
 		// draw border last, so not hidden by filled text
 		if (this.drawBorder)
 		{
 			context.strokeStyle = this.borderColor;
 			context.lineWidth = this.borderSize;
-			context.strokeText( this.text, this.x, this.y );
+			context.strokeText( this.text, this.position.x, this.position.y );
 		}		
 	}
 
